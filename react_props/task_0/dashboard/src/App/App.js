b6553b3baaec99274e721fb624d07be36243aadb
WoodsonTD <taylordwoodson@gmail.com>
2024-01-14 19:00:27 -0600

    add

--- react_props/task_0/dashboard/src/App/App.js
+++ react_props/task_0/dashboard/src/App/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./App.css";
-import Header from "../Header/Header";
+import Header from "../Header/Header.js";
 import Login from "../Login/Login";
-import Footer from "../Footer/Footer";
+import Footer from "../Footer/Footer.js";
 import Notifications from "../Notifications/Notifications";
 
 
