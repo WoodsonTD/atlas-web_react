6b1c7fa807a2fa131db67421cb5a3232f4b3535a
WoodsonTD <taylordwoodson@gmail.com>
2024-01-17 12:51:53 -0600

    add

---
+++ react_props/task_2/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import NotificationItem from './NotificationItem';
+
+const Notifications = () => {
+  // Dummy notifications data
+  const notifications = [
+    { id: 1, type: 'default', html: { __html: 'Notification 1' }, value: 'Notification 1' },
+    // Add more notifications as needed
+  ];
+
+  return (
+    <div>
+      <div className="menuItem">Your notifications</div>
+      <div className="Notifications">
+        {notifications.map((notification) => (
+          <NotificationItem key={notification.id} {...notification} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Notifications;
