2764214f1c7bbe8ba77ea5e3b925ab902c250cae
WoodsonTD <taylordwoodson@gmail.com>
2024-02-05 12:32:06 -0600

    task_0

---
+++ react_redux_reducer_selector/task_0/dashboard/src/schema/notifications.js
@@ -0,0 +1,32 @@
+import { normalize, schema } from 'normalizr';
+import data from '../../notifications.json';
+
+// Define the user entity
+const user = new schema.Entity('users');
+
+// Define the message entity
+const message = new schema.Entity('messages', {}, { idAttribute: 'guid' });
+
+// Define the notification entity
+const notification = new schema.Entity(
+    'notifications',
+    {
+        author: user,
+        context: message,
+    },
+    { idAttribute: 'id' }
+);
+
+// Normalize the data
+const normalizedData = normalize(data, [notification]);
+
+// Utility function to get all notifications by user using the normalized dataset
+const getAllNotificationsByUser = (userId) => {
+    const userNotifications = normalizedData.result.filter(
+        (notificationId) => notifications[notificationId].author.id === userId
+    );
+
+    return userNotifications.map((notificationId) => notifications[notificationId].context);
+};
+
+export { normalizedData, getAllNotificationsByUser };
